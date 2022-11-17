eb18e3b9a05385e4619d9c1e4c27c9ad0d38846e
Jose Motta <josemotta@bampli.com>
2022-11-17 12:49:31 -0300

    rev eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: '@loopback/eslint-config',
+  rules: {
+    "@typescript-eslint/camelCase": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/naming-convention": 0,
+  },
+  // ignorePatterns: ['config.js']
 };
